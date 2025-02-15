9572bcd6ea568ce0cc0e93b370133b6be4e0a091
JavierMerinoPinedo <jmerinopinedo@gmail.com>
2025-02-15 11:15:51 +0100

    [] - Actualizado index.php con la lectura de rutas.

--- Entrega01-TwitchAnalytics/index.php
+++ Entrega01-TwitchAnalytics/index.php
@@ -1,25 +1,37 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 include 'getUserById.php';
 include 'getStreams.php';
 include 'getEnrichedStreams.php';
+include 'register.php';
+include 'token.php';
 
 $uri = strtok($_SERVER["REQUEST_URI"], '?');
 header('Content-Type: application/json');
 
 if ($_SERVER['SERVER_NAME'] == 'localhost') {
     switch ($uri) {
-        case "/index.php/analytics/user":
+        case "/Entrega01-TwitchAnalytics/index.php/analytics/user":
             getUserById($_GET['id']);
             break;
-        case "/index.php/analytics/streams":
+        case "/Entrega01-TwitchAnalytics/index.php/analytics/streams":
             getStreams();
             break;
         case "/index.php/analytics/streams/enriched":
             getEnrichedStreams($_GET['limit']);
+            break;
+        case "/Entrega01-TwitchAnalytics/index.php/analytics/register":
+            $body = file_get_contents('php://input');
+            $data = json_decode($body, true);
+            $email = $data['email'];
+            register($email);
+            break;
+        case "/Entrega01-TwitchAnalytics/index.php/token":
+            $body = file_get_contents('php://input');
+            $data = json_decode($body, true);
+            $email = $data['email'];
+            $api_key = $data['api_key'];
+            token($email, $api_key);
             break;
     }
 } else{
@@ -33,6 +45,19 @@ if ($_SERVER['SERVER_NAME'] == 'localhost') {
         case "/analytics/streams/enriched":
             echo $_GET['limit'];
             getEnrichedStreams($_GET['limit']);
+            break;
+        case "/register":
+            $body = file_get_contents('php://input');
+            $data = json_decode($body, true);
+            $email = $data['email'];
+            register($email);
+            break;
+        case "/token":
+            $body = file_get_contents('php://input');
+            $data = json_decode($body, true);
+            $email = $data['email'];
+            $api_key = $data['api_key'];
+            token($email, $api_key);
             break;
     }
 }
