201cc4375f25593fea9f69b5c91bff92543efeca
JavierMerinoPinedo <jmerinopinedo@gmail.com>
2025-03-07 09:55:21 +0100

    [#39] - Corregir codigo getEnrichedStreams

--- funcionesAuxiliares/comprobarAuthorization.php
+++ funcionesAuxiliares/comprobarAuthorization.php
@@ -3,8 +3,8 @@
 function comprobarAuthorization(): void
 {
     if (!isset($_SERVER['HTTP_AUTHORIZATION'])) {
-        header("HTTP/1.1 400 Bad Request");
-        echo json_encode(['error' => "Authorization header is missing."], JSON_PRETTY_PRINT);
+        header('HTTP/1.1 400 Bad Request');
+        echo json_encode(['error' => 'Authorization header is missing.'], JSON_PRETTY_PRINT);
         exit();
     }
 }
