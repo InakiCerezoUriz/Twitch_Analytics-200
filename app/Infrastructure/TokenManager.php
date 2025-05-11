733a8e4425984016bdd6758c5dcf0ae0a6edb2e3
AlexNagore <alexnagoreiri@gmail.com>
2025-05-11 17:18:51 +0200

    [#75] - Desacoplado endpoint getUserById de algunas funciones auxiliares

---
+++ app/Infrastructure/TokenManager.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Infrastructure;
+
+use App\Repositories\DataBaseRepository;
+use App\Repositories\TwitchApiRepository;
+
+class TokenManager
+{
+    private DataBaseRepository $dataBaseRepository;
+    private TwitchApiRepository $twitchApiRepository;
+
+    public function __construct(
+        DataBaseRepository $dataBaseRepository,
+        TwitchApiRepository $twitchApiRepository
+    ) {
+        $this->dataBaseRepository  = $dataBaseRepository;
+        $this->twitchApiRepository = $twitchApiRepository;
+    }
+    public function generarToken(): array
+    {
+        $token      = bin2hex(random_bytes(16));
+        $expiracion = date('Y-m-d H:i:s', strtotime('+3 days'));
+        return ['token' => $token, 'expiracion' => $expiracion];
+    }
+
+    public function getToken(): string
+    {
+        [$fechaExpiracion, $token] = $this->dataBaseRepository->getApiTokenFromDataBase();
+
+        if (isset($token) && $fechaExpiracion > time()) {
+            return $token;
+        }
+        $response = $this->twitchApiRepository->getApiTokenFromApi();
+
+        $data = json_decode($response, true);
+
+        if (!empty($token)) {
+            $this->dataBaseRepository->updateApiTokenInDataBase($data);
+        } else {
+            $this->dataBaseRepository->insertApiTokenInDataBase($data);
+        }
+
+        return $data['access_token'];
+    }
+}
--- app/Repositories/TwitchApiRepository.php
+++ app/Repositories/TwitchApiRepository.php
@@ -4,18 +4,47 @@ namespace App\Repositories;
 
 class TwitchApiRepository
 {
-    public function getUserFromTwitchApi(string $id): array
+    public function getUserFromTwitchApi(string $id, string $token): array
     {
         $api_url = 'https://api.twitch.tv/helix/users?id=' . $id;
-        $token   = conseguirToken();
 
         $headers = [
-            "Authorization: Bearer $token",  // Token
+            'Authorization: Bearer ' . $token,  // Token
             'Client-Id: pdp08hcdlqz3u2l18wz5eeu6kyll93',  // Client ID de la aplicación de twitch
             'Content-Type: application/json',
         ];
 
-        [$response, $res] = iniciarCurl($api_url, $headers);
+        $ch = curl_init($api_url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+        $res      = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        curl_close($ch);
+
         return [$response, $res];
+    }
+
+    public function getApiTokenFromApi(): bool|string
+    {
+        $api_url = 'https://id.twitch.tv/oauth2/token';
+
+        $post_fields = http_build_query([
+            'client_id'     => env('TWITCH_CLIENT_ID'),
+            'client_secret' => env('TWITCH_CLIENT_SECRET'),
+            'grant_type'    => 'client_credentials',
+        ]);
+
+        $ch = curl_init($api_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $response;
     }
 }
