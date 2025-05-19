2b6f9b54bce6dc4f8e7cabc81fa7d43c24f19545
Javier <117859441+JavierMerinoPinedo@users.noreply.github.com>
2025-05-19 10:27:02 +0200

    [#88] - Desacoplada getStreams (#115)
    * [#88] - Desacoplada getStreams
    
    * [#88] - Cambio en el constructor

--- app/Http/Controllers/GetStreams/GetStreamsController.php
+++ app/Http/Controllers/GetStreams/GetStreamsController.php
@@ -9,19 +9,12 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class GetStreamsController extends BaseController
 {
-    private GetStreamsService $getStreamsService;
     public function __construct(
-        GetStreamsService $getStreamsService
+        private readonly GetStreamsService $getStreamsService
     ) {
-        $this->getStreamsService = $getStreamsService;
     }
     public function getStreams(Request $request): JsonResponse
     {
-        require_once __DIR__ . '/../../../../src/funcionesAuxiliares/conseguirToken.php';
-        require_once __DIR__ . '/../../../../src/funcionesAuxiliares/iniciarCurl.php';
-        require_once __DIR__ . '/../../../../src/funcionesAuxiliares/comprobarExpiracion.php';
-        require_once __DIR__ . '/../../../../src/funcionesAuxiliares/comprobarAuthorization.php';
-
         return $this->getStreamsService->getStreams();
     }
 }
