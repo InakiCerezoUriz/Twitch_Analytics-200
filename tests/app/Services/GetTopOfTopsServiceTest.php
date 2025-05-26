f14d39c845e137f061e6592db5a9f84e38ab26d0
Beñat <150382660+Bernardeiro@users.noreply.github.com>
2025-05-26 16:35:37 +0200

    [#78] - Añadidos test de servicio de TopOfTops (#133)
    * [#78] - Añadidos test de servicio de TopOfTops
    
    * [#78] - Añadidos test funcionales de servicio de TopOfTops

---
+++ tests/app/Services/GetTopOfTopsServiceTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace TwitchAnalytics\Tests\app\Services;
+
+use App\Infrastructure\TokenManager;
+use App\Interfaces\DataBaseRepositoryInterface;
+use App\Interfaces\TwitchApiRepositoryInterface;
+use App\Services\GetTopOfTopsService;
+use PHPUnit\Framework\TestCase;
+
+class GetTopOfTopsServiceTest extends TestCase
+{
+    private DataBaseRepositoryInterface $dataBaseRepository;
+    private TwitchApiRepositoryInterface $twitchApiRepository;
+    private TokenManager $tokenManager;
+    private GetTopOfTopsService $service;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->dataBaseRepository  = $this->createMock(DataBaseRepositoryInterface::class);
+        $this->twitchApiRepository = $this->createMock(TwitchApiRepositoryInterface::class);
+        $this->tokenManager        = $this->createMock(TokenManager::class);
+
+        $this->service = new GetTopOfTopsService(
+            $this->dataBaseRepository,
+            $this->twitchApiRepository,
+            $this->tokenManager
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function returns404WhenTopGamesIsEmpty(): void
+    {
+        $this->tokenManager->method('getToken')->willReturn('valid_token');
+
+        $this->twitchApiRepository->method('getTopGames')->willReturn([json_encode(['data' => []])]);
+
+        $response = $this->service->getTopOfTops(null);
+
+        $this->assertEquals(404, $response->getStatusCode());
+        $this->assertEquals(['error' => 'No top games data available.'], $response->getData(true));
+    }
+
+    /**
+     * @test
+     */
+    public function returns200WithCachedTopStreamers(): void
+    {
+        $this->tokenManager->method('getToken')->willReturn('valid_token');
+
+        $topGames = [['id' => '1', 'name' => 'Game 1']];
+        $this->twitchApiRepository->method('getTopGames')->willReturn([json_encode(['data' => $topGames])]);
+
+        $this->dataBaseRepository->method('getUltimaSolicitud')->willReturn(time());
+        $this->dataBaseRepository->method('getTopStreamer')->willReturn([
+            ['name' => 'TopStreamer', 'game' => 'Game 1'],
+        ]);
+
+        $response = $this->service->getTopOfTops('600');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals([['name' => 'TopStreamer', 'game' => 'Game 1']], $response->getData(true));
+    }
+}
