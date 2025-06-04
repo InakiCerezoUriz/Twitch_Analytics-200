64d0aad4139483a43264591d474fd9a1735f485b
Iñaki Cerezo <cerezo.147574@e.unavarra.es>
2025-06-04 21:57:19 +0200

    [#148] - Test givenIdThatIsNotInTheDataBaseReturns200

--- tests/Integration/Http/Controllers/GetUserById/GetUserByIdControllerTest.php
+++ tests/Integration/Http/Controllers/GetUserById/GetUserByIdControllerTest.php
@@ -2,10 +2,11 @@
 
 namespace TwitchAnalytics\Tests\Integration\Http\Controllers\GetUserById;
 
-use Illuminate\Http\JsonResponse;
 use TwitchAnalytics\Exceptions\EmptyOrInvalidIdException;
 use TwitchAnalytics\Http\Controllers\GetUserById\GetUserByIdValidator;
-use TwitchAnalytics\Services\GetUserByIdService;
+use TwitchAnalytics\Infrastructure\TokenManager;
+use TwitchAnalytics\Interfaces\DataBaseRepositoryInterface;
+use TwitchAnalytics\Interfaces\TwitchApiRepositoryInterface;
 use TwitchAnalytics\Tests\TestCase;
 
 class GetUserByIdControllerTest extends TestCase
@@ -61,40 +62,74 @@ class GetUserByIdControllerTest extends TestCase
     /**
      * @test
      */
-    public function givenIdReturns200(): void
+    public function givenIdThatIsNotInTheDataBaseReturns200(): void
     {
-        $mockResponse = new JsonResponse([
-            'id',
-            'login',
-            'display_name',
-            'type',
-            'broadcaster_type',
-            'description',
-            'profile_image_url',
-            'offline_image_url',
-            'view_count',
-            'created_at',
-        ], 200);
+        $mockResponse = [
+            'id'                => 'id',
+            'login'             => 'login',
+            'display_name'      => 'display_name',
+            'type'              => 'type',
+            'broadcaster_type'  => 'broadcaster_type',
+            'description'       => 'description',
+            'profile_image_url' => 'profile_image_url',
+            'offline_image_url' => 'offline_image_url',
+            'view_count'        => 'view_count',
+            'created_at'        => 'created_at',
+        ];
+        $mockResponse = ['data' => [$mockResponse]];
 
-        $mockService = \Mockery::mock(GetUserByIdService::class);
-        $mockService->shouldReceive('getUser')
+
+        $mockTokenManager = \Mockery::mock(TokenManager::class);
+        $mockTokenManager->shouldReceive('getToken')
             ->once()
-            ->with('1')
-            ->andReturn($mockResponse);
+            ->andReturn('valid-token');
 
-        $this->app->instance(GetUserByIdService::class, $mockService);
+        $mockTokenManager->shouldReceive('tokenActive')
+            ->once()
+            ->andReturn(true);
+
+
+        $mockUserValidator = \Mockery::mock(GetUserByIdValidator::class);
+        $mockUserValidator->shouldReceive('validateId')
+            ->once()
+            ->with('valid_id')
+            ->andReturn('valid_id');
+
+
+        $mockDataBase = \Mockery::mock(DataBaseRepositoryInterface::class);
+        $mockDataBase->shouldReceive('getUserFromDataBase')
+            ->once()
+            ->with('valid_id')
+            ->andReturn(null);
+        $mockDataBase->shouldReceive('insertUserInDataBase')
+            ->once()
+            ->with($mockResponse['data'][0])
+            ->andReturn(null);
+
+
+        $mockTwitchApiRepo = \Mockery::mock(TwitchApiRepositoryInterface::class);
+        $mockTwitchApiRepo->shouldReceive('getUserFromTwitchApi')
+            ->once()
+            ->with('valid_id', 'valid-token')
+            ->andReturn([json_encode($mockResponse),200]);
+
+
+        $this->app->instance(TokenManager::class, $mockTokenManager);
+        $this->app->instance(GetUserByIdValidator::class, $mockUserValidator);
+        $this->app->instance(TwitchApiRepositoryInterface::class, $mockTwitchApiRepo);
+        $this->app->instance(DataBaseRepositoryInterface::class, $mockDataBase);
 
         $response = $this->call(
             'GET',
             '/analytics/user',
-            ['id' => '1'],
+            ['id' => 'valid_id'],
             [],
             [],
             ['HTTP_AUTHORIZATION' => 'Bearer 8ac4f576cf1297671c3acd79bd7aa344']
         );
 
         $response->assertStatus(200);
-        $response->assertJson([
+        $response->assertJsonStructure([
             'id',
             'login',
             'display_name',
