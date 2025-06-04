5ff9b314e6604d51c3c0552b4b335697ef3bf3e6
Iñaki Cerezo <cerezo.147574@e.unavarra.es>
2025-06-04 22:14:08 +0200

    [#148] - Test givenIdThatIsInTheDataBaseReturns200

--- tests/Integration/Http/Controllers/GetUserById/GetUserByIdControllerTest.php
+++ tests/Integration/Http/Controllers/GetUserById/GetUserByIdControllerTest.php
@@ -142,4 +142,76 @@ class GetUserByIdControllerTest extends TestCase
             'created_at',
         ]);
     }
+
+    /**
+     * @test
+     */
+    public function givenIdThatIsInTheDataBaseReturns200()
+    {
+        $mockResponse = [
+            'id'                => 'id',
+            'login'             => 'login',
+            'display_name'      => 'display_name',
+            'type'              => 'type',
+            'broadcaster_type'  => 'broadcaster_type',
+            'description'       => 'description',
+            'profile_image_url' => 'profile_image_url',
+            'offline_image_url' => 'offline_image_url',
+            'view_count'        => 'view_count',
+            'created_at'        => 'created_at',
+        ];
+        $mockResponse = ['data' => [$mockResponse]];
+
+
+        $mockTokenManager = \Mockery::mock(TokenManager::class);
+        $mockTokenManager->shouldReceive('getToken')
+            ->once()
+            ->andReturn('valid-token');
+
+        $mockTokenManager->shouldReceive('tokenActive')
+            ->once()
+            ->andReturn(true);
+
+
+        $mockUserValidator = \Mockery::mock(GetUserByIdValidator::class);
+        $mockUserValidator->shouldReceive('validateId')
+            ->once()
+            ->with('valid_id')
+            ->andReturn('valid_id');
+
+
+        $mockDataBase = \Mockery::mock(DataBaseRepositoryInterface::class);
+        $mockDataBase->shouldReceive('getUserFromDataBase')
+            ->once()
+            ->with('valid_id')
+            ->andReturn($mockResponse['data'][0]);
+
+
+        $this->app->instance(TokenManager::class, $mockTokenManager);
+        $this->app->instance(GetUserByIdValidator::class, $mockUserValidator);
+        $this->app->instance(DataBaseRepositoryInterface::class, $mockDataBase);
+
+        $response = $this->call(
+            'GET',
+            '/analytics/user',
+            ['id' => 'valid_id'],
+            [],
+            [],
+            ['HTTP_AUTHORIZATION' => 'Bearer 8ac4f576cf1297671c3acd79bd7aa344']
+        );
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'id',
+            'login',
+            'display_name',
+            'type',
+            'broadcaster_type',
+            'description',
+            'profile_image_url',
+            'offline_image_url',
+            'view_count',
+            'created_at',
+        ]);
+    }
 }
